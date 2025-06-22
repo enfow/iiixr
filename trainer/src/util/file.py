d49d72272adee4b866a16ce37c7395d276480865
enfow <wgm0601@gmail.com>
2025-06-22 13:33:58 +0900

    [trainer] introduce util/file.py

---
+++ trainer/src/util/file.py
@@ -0,0 +1,19 @@
+import json
+from typing import Union
+
+from schema.result import EvalResult, TrainResult
+
+
+def save_json(data: dict, file_path: str):
+    with open(file_path, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def log_result(result: Union[TrainResult, EvalResult], file_path: str):
+    json_dumped = json.dumps(result.to_dict()) + "\n"
+    save_json(json_dumped, file_path)
+
+
+def load_json(file_path: str):
+    with open(file_path, "r") as f:
+        return json.load(f)
